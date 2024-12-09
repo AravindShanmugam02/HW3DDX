92dd8d77c2adfbc198200984407be77c45bfb1bb
Aravind Shanmugam <aravind.shanmugam@ubisoft.com>
2024-12-09 13:31:47 +0000

    ashan: [Add] Custom Window Procedure ashan: [Add] Message Pump ashan: [Modify] Exit code

--- HW3DDX/WinMain.cpp
+++ HW3DDX/WinMain.cpp
@@ -1,5 +1,20 @@
 #include <Windows.h>
 
+// Custom window procedure to handle multiple messages and
+// to define the behavior of the created window that is in context.
+LRESULT CALLBACK CustWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(99);
+		break;
+	}
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 	// To create a window class
@@ -10,7 +25,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
 	// Structure configuration part
 	winClass.style = CS_OWNDC;
-	winClass.lpfnWndProc = DefWindowProc;
+	winClass.lpfnWndProc = CustWndProc;
 	winClass.cbClsExtra = 0;
 	winClass.cbWndExtra = 0;
 	winClass.hInstance = hInstance;
@@ -36,7 +51,32 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
 	// To show the window
 	ShowWindow(hWindow, SW_SHOW);
+
+	// To make a message pump
+	// 1. First, need a MSG
+	MSG winMessage;
 	
-	while (true);
-	return 0;
+	// To handle different MSG code that is returned from GetMessage().
+	// Therefore, we declare a BOOL to hold the returned code.
+	BOOL winRetCode;
+	
+	// We use while so that the window stays on as long as we are quitting or it runs into error.
+	while (// 2. Second, pass the MSG to GetMessage()
+		(winRetCode = GetMessage(&winMessage, nullptr, 0, 0)) > 0)
+	{
+		// 3. Translate the MSG. (ashanmugam [TODO] To Find More?)
+		TranslateMessage(&winMessage);
+		// 4. Dispatch the MSG to Win32's window procedure for the window created.
+		DispatchMessage(&winMessage);
+	}
+
+	if (winRetCode == -1)
+	{
+		// (ashanmugam [TODO] To handle error)
+		return -1;
+	}
+	else
+	{
+		return winMessage.wParam;
+	}
 }
